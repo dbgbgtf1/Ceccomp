7c0bff75b0cd53d538e51fab03f2ed5bcfa49d8a
dbgbgtf <dudududuMaxVer@outlook.com>
2025-05-26 00:32:55 +0800

    format help msg

--- include/help.h
+++ include/help.h
@@ -3,7 +3,7 @@
 #define DISASM_HINT "ceccomp disasm  [ -a ARCH ] [ raw ]"
 
 #define EMU_HINT                                                              \
-  "ceccomp emu     [ -a ARCH ] [ -q ] [ text ] syscall_nr [ args[0-5] ip ]"
+  "ceccomp emu     [ -a ARCH ] [ -q ] text syscall_nr [ args[0-5] ip ]"
 
 #define PROBE_HINT                                                            \
   "ceccomp probe   [ -a ARCH ] [ -o FILE ] PROGRAM [ program-args ]"
@@ -14,30 +14,38 @@
 
 #define OPTION_HINT                                                           \
   "Options:\n" OPTION_ARCH_HINT OPTION_FMT_HINT OPTION_PID_HINT               \
-      OPTION_OUTPUT_HINT OPTION_QUIET_HINT ARG_SYSCALL_NR_HINT OPTION_ARG_IP_HINT            \
-          OPTION_FILE_HINT
+      OPTION_OUTPUT_HINT OPTION_QUIET_HINT ARG_SYSCALL_NR_HINT                \
+          OPTION_ARG_IP_HINT OPTION_FILE_HINT
 
 #define OPTION_ARCH_HINT                                                      \
-  "-a, --arch (x86_64|aarch64|...)  Which architecture to resolve syscall_nr, default as "       \
-  "your arch\n"
+  "-a, --arch (x86_64|aarch64|...)  Which architecture to resolve "           \
+  "syscall_nr, default as your arch\n"
+
 #define OPTION_FMT_HINT                                                       \
   "-f, --fmt (hexline|hexfmt|raw)   Output format, default as hexline\n"
+
 #define OPTION_PID_HINT                                                       \
-  "-p, --pid system_process_id      Extract bpf filters from process and print " \
-  "with bpf text form; CAP_SYS_ADMIN is needed to work\n"
+  "-p, --pid system_process_id      Extract bpf filters from process and "    \
+  "print with bpf text form; CAP_SYS_ADMIN is needed to work\n"
+
 #define OPTION_OUTPUT_HINT                                                    \
-  "-o, --output file                Print to file to avoid mixing ceccomp output and "      \
-  "tracee program output, default as stderr\n"
+  "-o, --output file                Print to file to avoid mixing ceccomp "   \
+  "output and tracee program output, default as stderr\n"
+
 #define OPTION_QUIET_HINT                                                     \
   "-q, --quiet                      Print emulate result only\n"
-#define ARG_SYSCALL_NR_HINT \
-  "syscall_nr                       System call number or name (e.g. 0|read)\n"
+
+#define ARG_SYSCALL_NR_HINT                                                   \
+  "syscall_nr                       System call number or name (e.g. "        \
+  "0|read)\n"
+
 #define OPTION_ARG_IP_HINT                                                    \
-  "args[0-5], ip                    args and ip (instruction pointer) used "   \
+  "args[0-5], ip                    args and ip (instruction pointer) used "  \
   "for emulation, default as 0\n"
+
 #define OPTION_FILE_HINT                                                      \
-  "raw, text                        File with BPF RAW or BPF TEXT, see docs for detail, default "   \
-  "as stdin\n"
+  "raw, text                        File with BPF RAW or BPF TEXT, see docs " \
+  "for detail, default as stdin\n"
 
 #define HELP_HINT "ceccomp help"
 #define VERSION "ceccomp version"
