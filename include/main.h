56af1112c9e09298c4b68488e11974c4478a52cb
RocketDev <ma2014119@outlook.com>
2025-08-08 16:51:54 +0800

    bump: version 2.9

--- include/main.h
+++ include/main.h
@@ -11,7 +11,7 @@
 #include "help.h"
 // clang-format on
 
-#define CECCOMP_VERSION "ceccomp 2.8"
+#define CECCOMP_VERSION "ceccomp 2.9"
 
 typedef struct sock_fprog fprog;
 typedef struct sock_filter filter;
