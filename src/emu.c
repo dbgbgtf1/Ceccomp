84acd7448bf610078c13ae95dd2fafafb7233858
dbgbgtf <dudududuMaxVer@outlook.com>
2025-09-27 21:42:19 +0800

    fix(emu): lol I just wrote a stupid bug

--- src/emu.c
+++ src/emu.c
@@ -293,7 +293,7 @@ emu_lines (bool quiet, FILE *read_fp, seccomp_data *data)
 
       if (read_idx < execute_idx)
         {
-          pre_clear_color (Line.origin_start);
+          pre_clear_color (origin_line);
           LIGHTCOLORPRINTF (FORMAT ": %s", read_idx, Line.origin_start);
           continue;
         }
