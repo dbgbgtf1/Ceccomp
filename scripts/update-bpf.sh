8419c301cbf9ad347541f74a31b2e2fb47d6f5a3
dbgbgtf <dudududuMaxVer@outlook.com>
2025-11-02 16:36:44 +0800

    chore(test): mv update-text.sh -> update-bpf.sh, add error msg

--- scripts/update-bpf.sh
+++ scripts/update-bpf.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-override_text()
+override_bpf()
 {
-    ./build/ceccomp disasm -c always $1 > $2
+    ./build/ceccomp asm -c always -f raw $1 > $2 2>&1
 }
 
-for bpf in test/bpf/*.bpf; do
-    text=${bpf%.bpf}
-    text=${text/bpf/text}
-    override_text $bpf $text
+for txt in test/text/*; do
+    bpf=${txt/text/bpf}
+    bpf=${bpf}.bpf
+    override_bpf $txt $bpf
 done
 
 override_emu()
 {
-    ./build/ceccomp emu -c always $1 $2 1 2 3 4 5 6 > $3
+    ./build/ceccomp emu -c always $1 $2 1 2 3 4 5 6 > $3 2>&1
 }
 
 for text in test/text/*; do
