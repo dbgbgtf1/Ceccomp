03315944bc0806b248ae1de145b9788321a07e74
RocketDev <ma2014119@outlook.com>
2025-11-08 02:26:13 +0800

    feat(test): add tolerant to at least test all cases
    refactor code to display error message and error handling

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 export LC_ALL=C
 
+TOLERANT=""
+if [ "$1" == "--tolerant" ]; then
+    TOLERANT=1
+fi
+errors=0
+
 # skip chromium if libseccomp lower than 2.5.6
 EXPECT_VER=2.5.6
+TOO_OLD_MSG="libseccomp too old (<2.5.6)"
 SKIP_CHROMIUM=""
-if [ $(echo "$(pkg-config --modversion libseccomp)\n$EXPECT_VER" | sort | head -n1) != "$EXPECT_VER" ]; then
+if [ $(echo -e "$(pkg-config --modversion libseccomp)\n$EXPECT_VER" | sort | head -n1) != "$EXPECT_VER" ]; then
     SKIP_CHROMIUM=1
 fi
+
+skip_test ()
+{
+    echo "[-] $filename skipped, $1"
+}
 
 check_pass ()
 {
     if [ $1 -eq 0 ]; then
         echo "[+] $filename passed"
     else
-        echo "[x] $filename failed, cat diff_result for details"
-        exit 1
+        echo "[x] $filename failed"
+        if [ -n "$TOLERANT" ]; then
+            ((errors += 1))
+        else
+            exit 1
+        fi
     fi
 }
 
@@ -28,10 +44,10 @@ for file in "${files[@]}"; do
   if [ -f "$file" ]; then
     filename=$(basename -s .bpf "$file")
     if [ -n "$SKIP_CHROMIUM" ] && [ "$filename" == "chromium" ]; then
-        echo "[-] libseccomp too old, skip $filename"
+        skip_test "$TOO_OLD_MSG"
         continue
     fi
-    diff <(./build/ceccomp disasm $file --color always -a x86_64) ./test/text/$filename > ./build/diff_result
+    diff <(./build/ceccomp disasm $file --color always -a x86_64) ./test/text/$filename
     check_pass $?
   fi
 done
@@ -46,10 +62,10 @@ for file in "${files[@]}"; do
   if [ -f "$file" ]; then
     filename=$(basename "$file")
     if [ -n "$SKIP_CHROMIUM" ] && [ "$filename" == "chromium" ]; then
-        echo "[-] libseccomp too old, skip $filename"
+        skip_test "$TOO_OLD_MSG"
         continue
     fi
-    diff <(./build/ceccomp asm $file --fmt raw -a x86_64) test/bpf/$filename.bpf > ./build/diff_result
+    diff <(./build/ceccomp asm $file --fmt raw -a x86_64) test/bpf/$filename.bpf
     check_pass $?
   fi
 done
@@ -62,14 +78,14 @@ for file in "${files[@]}"; do
   if [ -f "$file" ]; then
     filename=$(basename "$file")
     if [ -n "$SKIP_CHROMIUM" ] && [ "$filename" == "chromium" ]; then
-        echo "[-] libseccomp too old, skip $filename"
+        skip_test "$TOO_OLD_MSG"
         continue
     fi
-    diff <(./build/ceccomp emu -c always -a x86_64 $file open 1 2 3 4 5 6) ./test/emu_result/$filename.open > ./build/diff_result
+    diff <(./build/ceccomp emu -c always -a x86_64 $file open 1 2 3 4 5 6) ./test/emu_result/$filename.open
     check_pass $?
-    diff <(./build/ceccomp emu -c always -a x86_64 $file pipe 1 2 3 4 5 6) ./test/emu_result/$filename.pipe > ./build/diff_result
+    diff <(./build/ceccomp emu -c always -a x86_64 $file pipe 1 2 3 4 5 6) ./test/emu_result/$filename.pipe
     check_pass $?
-    diff <(./build/ceccomp emu -c always -a x86_64 $file accept 1 2 3 4 5 6) ./test/emu_result/$filename.accept > ./build/diff_result
+    diff <(./build/ceccomp emu -c always -a x86_64 $file accept 1 2 3 4 5 6) ./test/emu_result/$filename.accept
     check_pass $?
   fi
 done
@@ -83,7 +99,7 @@ files=(./test/errors/*.bpf)
 for file in "${files[@]}"; do
   if [ -f "$file" ]; then
     filename=$(basename "$file")
-    diff <(./build/ceccomp disasm -c always -a x86_64 $file 2>&1) ./test/errors/$filename.err > ./build/diff_result
+    diff <(./build/ceccomp disasm -c always -a x86_64 $file 2>&1) ./test/errors/$filename.err
     check_pass $?
   fi
 done
@@ -96,12 +112,12 @@ make test
 
 filename="trace test"
 timeout 0.2 ./build/ceccomp trace -o ./build/dyn_result -c always ./build/test &>/dev/null
-diff ./build/dyn_result ./test/trace.log > ./build/diff_result
+diff ./build/dyn_result ./test/trace.log
 check_pass $?
 
 filename="probe test"
 ./build/ceccomp probe -o ./build/dyn_result -c always ./build/test &>/dev/null
-diff ./build/dyn_result ./test/probe.log > ./build/diff_result
+diff ./build/dyn_result ./test/probe.log
 check_pass $?
 
 if [ "z$(pgrep -f '^./build/test$')z" != "zz" ]; then
@@ -110,4 +126,13 @@ if [ "z$(pgrep -f '^./build/test$')z" != "zz" ]; then
   exit 1
 else
   echo "[+] ptrace jail works"
+fi
+
+echo ""
+echo "=====summary====="
+if [ $errors -eq 0 ]; then
+    echo "[+] ALL checks passed!"
+else
+    echo "[x] $errors check(s) failed!"
+    exit 1
 fi
