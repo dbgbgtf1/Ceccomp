cf1cf5dbe2638e74b3677cede1b37acb3defdab4
dbgbgtf <dudududuMaxVer@outlook.com>
2025-12-15 20:17:51 +0800

    refactor(decode_filters): disasm can use decode_filter to transfer *bpf* to *ir*

---
+++ include/decode_filters.h
@@ -0,0 +1,10 @@
+#ifndef DECODE_FILTERS
+#define DECODE_FILTERS
+
+#include "main.h"
+#include "vector.h"
+
+// return statement_t vector
+extern void decode_filters (fprog *prog, vector_t *v);
+
+#endif
