b76d9f31ec6175121563f5ac76afb44dd966bf7f
RocketDev <ma2014119@outlook.com>
2026-01-19 02:46:03 +0800

    refactor(hash): format code

--- src/utils/hash.c
+++ src/utils/hash.c
@@ -9,18 +9,22 @@
 
 #include "lib/a5hash-5.25.h"
 
-static uint64_t hash_hkey (hkey_t *key) {
+static uint64_t
+hash_hkey (hkey_t *key)
+{
 #if __SIZEOF_POINTER__ == 8
-    return a5hash(key->start, key->len, 0);
+  return a5hash (key->start, key->len, 0);
 #else
-    return a5hash32(key->start, key->len, 0);
+  return a5hash32 (key->start, key->len, 0);
 #endif
 }
 
-static bool cmpr_hkey(hkey_t *key1, hkey_t *key2) {
-    if (key1->len != key2->len)
-        return false;
-    return !memcmp(key1->start, key2->start, key1->len);
+static bool
+cmpr_hkey (hkey_t *key1, hkey_t *key2)
+{
+  if (key1->len != key2->len)
+    return false;
+  return !memcmp (key1->start, key2->start, key1->len);
 }
 
 #define NAME str_table
@@ -35,32 +39,32 @@ str_table hash_table;
 int
 insert_key (hkey_t *key, uint16_t line_nr)
 {
-    size_t prev_size = vt_size(&hash_table);
-    str_table_itr itr = vt_get_or_insert(&hash_table, key, line_nr);
-    if (vt_is_end(itr))
-        return -1;
-    if (prev_size == vt_size(&hash_table))
-        return 1;
-    return 0;
+  size_t prev_size = vt_size (&hash_table);
+  str_table_itr itr = vt_get_or_insert (&hash_table, key, line_nr);
+  if (vt_is_end (itr))
+    return -1;
+  if (prev_size == vt_size (&hash_table))
+    return 1;
+  return 0;
 }
 
 uint16_t
 find_key (hkey_t *key)
 {
-    str_table_itr itr = vt_get(&hash_table, key);
-    if (vt_is_end(itr))
-        error(M_CANNOT_FIND_LABEL, key->len, key->start);
-    return itr.data->val;
+  str_table_itr itr = vt_get (&hash_table, key);
+  if (vt_is_end (itr))
+    error (M_CANNOT_FIND_LABEL, key->len, key->start);
+  return itr.data->val;
 }
 
 void
 init_table ()
 {
-    vt_init(&hash_table);
+  vt_init (&hash_table);
 }
 
 void
 free_table ()
 {
-    vt_cleanup(&hash_table);
+  vt_cleanup (&hash_table);
 }
