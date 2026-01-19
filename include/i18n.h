1d4dab4185827a465cd5b4c36f11a423fb0c0141
RocketDev <ma2014119@outlook.com>
2026-01-19 22:59:35 +0800

    feat(i18n): embed localedir in config.h

--- include/i18n.h
+++ include/i18n.h
@@ -1,9 +1,11 @@
+#ifndef CONFIG_H
+#include "config.h"
+#endif
 #ifndef I18N_H
 #define I18N_H
 
 #ifdef LOCALEDIR
 #include <libintl.h>
-#include <locale.h>
 #define _(string) gettext (string)
 #else
 #define _(string) string
