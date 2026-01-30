5c63d492e12934521c1b1f073bc27c2cc9fb3c24
RocketDev <ma2014119@outlook.com>
2026-01-30 16:59:30 +0800

    feat(test): use bpf code to fix big endian loading

--- test/unit_test.c
+++ test/unit_test.c
@@ -1,6 +1,7 @@
 // this is for separately function testing
 #include "main.h"
 #include <linux/audit.h>
+#include <linux/bpf_common.h>
 #include <linux/filter.h>
 #include <linux/seccomp.h>
 #include <signal.h>
@@ -16,23 +17,24 @@
 
 #define ARRAY_SIZE(arr) (sizeof (arr) / sizeof (arr[0]))
 
+static const filter filters[] = {
+  BPF_STMT (BPF_LD | BPF_W | BPF_ABS, (offsetof (seccomp_data, nr))),
+  BPF_JUMP (BPF_JMP | BPF_JEQ | BPF_K, SYS_execve, 1, 0),
+  BPF_STMT (BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+  BPF_STMT (BPF_RET | BPF_K, SECCOMP_RET_ERRNO | 1),
+};
+
 static void
 load_filter (void)
 {
   prctl (PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
-  /* Assume that AUDIT_ARCH_X86_64 means the normal x86-64 ABI
-     (in the x32 ABI, all system calls have bit 30 set in the
-     'nr' field, meaning the numbers are >= X32_SYSCALL_BIT). */
-
-  char f[] = "\x20\x00\x00\x00\x00\x00\x00\x00"
-             "\x15\x00\x01\x00\xff\xff\xff\xff"
-             "\x06\x00\x00\x00\x00\x00\xff\x7f"
-             "\x06\x00\x00\x00\x01\x00\x05\x00";
-
   struct sock_fprog prog
-      = { .len = ARRAY_SIZE (f) / sizeof (filter), .filter = (filter *)f };
+      = { .len = ARRAY_SIZE (filters), .filter = (filter *)filters };
 
   syscall (SYS_seccomp, SECCOMP_SET_MODE_FILTER, NULL, &prog);
+
+  // test failed loading
+  syscall (SYS_seccomp, SECCOMP_SET_MODE_FILTER, NULL, NULL);
 }
 
 int
